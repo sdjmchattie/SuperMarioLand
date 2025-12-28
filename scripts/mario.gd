f18063a4b9149f021d8ba3c3937c0125f24b411b
Stuart McHattie <stuart.mchattie@gmail.com>
2025-12-28 21:14:52 +0000

    Do some quality improvements and camera tracking

--- scripts/mario.gd
+++ scripts/mario.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
+@export var camera: Camera2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 60.0
+const JUMP_VELOCITY = -250.0
+const HALF_WIDTH = 80.0
+const LEFT_MARGIN = 4.0
 
+@onready var sprite: Sprite2D = $SmallSprite
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +15,23 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
+		sprite.flip_h = direction < 0
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	# Stop at left margin
+	if global_position.x + velocity.x * delta <= camera.global_position.x + LEFT_MARGIN:
+		velocity.x = 0
+
 	move_and_slide()
+
+	if global_position.x > camera.global_position.x + HALF_WIDTH:
+		camera.global_position.x = global_position.x - HALF_WIDTH
