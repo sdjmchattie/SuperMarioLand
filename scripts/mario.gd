225c60220de5c3b635074386df37a7dd88343a3b
Stuart McHattie <stuart.mchattie@gmail.com>
2025-12-29 21:24:26 +0000

    Fine tune jump and fall parameters

--- scripts/mario.gd
+++ scripts/mario.gd
@@ -11,13 +11,14 @@ enum MarioState {
 }
 
 @export var camera: Camera2D
-@export var terminal_jump_speed := 120.0
-@export var terminal_ledge_fall_speed := 250.0
-@export var apex_gravity := 700.0
-@export var max_jump_time := 0.18
+@export var terminal_jump_speed := 110.0
+@export var terminal_ledge_fall_speed := 175.0
+@export var apex_gravity := 550.0
+@export var max_jump_time := 0.2
 @export var horizontal_speed = 60.0
 
 @onready var sprite: Sprite2D = $SmallSprite
+@onready var animator: AnimationPlayer = $AnimationPlayer
 
 var mario_state: MarioState = MarioState.ON_GROUND
 var jump_timer = 0.0
@@ -31,9 +32,9 @@ func _physics_process(delta: float) -> void:
 		MarioState.JUMP_APEX:
 			_physics_jump_apex(delta)
 		MarioState.JUMP_DESCENT:
-			_physics_jump_descent(delta)
+			_physics_descent(delta)
 		MarioState.LEDGE_DESCENT:
-			_physics_ledge_descent(delta)
+			_physics_descent(delta)
 		MarioState.PIPE_ENTRY:
 			pass
 		MarioState.DEAD:
@@ -66,25 +67,40 @@ func _check_grounded() -> void:
 		velocity.y = 0.0
 		mario_state = MarioState.ON_GROUND
 
-func _physics_on_ground(delta: float) -> void:
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -1 * terminal_jump_speed
-		jump_timer = 0.0
-		mario_state = MarioState.JUMP_ASCENT
-	elif not is_on_floor():
-		velocity.y = terminal_ledge_fall_speed
-		mario_state = MarioState.LEDGE_DESCENT
+func _transition_jump_ascent() -> void:
+	mario_state = MarioState.JUMP_ASCENT
+	animator.play("jump")
+	velocity.y = -1 * terminal_jump_speed
+	jump_timer = 0.0
 
+func _transition_ledge_descent() -> void:
+	mario_state = MarioState.LEDGE_DESCENT
+	animator.pause()
+	velocity.y = terminal_ledge_fall_speed
+
+func _physics_on_ground(delta: float) -> void:
 	_horizontal_movement(delta)
 	move_and_slide()
 
+	if velocity.x == 0:
+		animator.play("idle")
+	else:
+		animator.play("walk")
+
+	if Input.is_action_just_pressed("jump"):
+		_transition_jump_ascent()
+	elif not is_on_floor():
+		_transition_ledge_descent()
+
+
 func _physics_jump_ascent(delta: float) -> void:
+	_horizontal_movement(delta)
+	move_and_slide()
+
 	jump_timer += delta
 	if not Input.is_action_pressed("jump") or jump_timer >= max_jump_time:
 		mario_state = MarioState.JUMP_APEX
 
-	_horizontal_movement(delta)
-	move_and_slide()
 
 func _physics_jump_apex(delta: float) -> void:
 	velocity.y += apex_gravity * delta
@@ -92,16 +108,13 @@ func _physics_jump_apex(delta: float) -> void:
 		velocity.y = terminal_jump_speed
 		mario_state = MarioState.JUMP_DESCENT
 
-	_check_grounded()
 	_horizontal_movement(delta)
 	move_and_slide()
 
-func _physics_jump_descent(delta: float) -> void:
 	_check_grounded()
+
+func _physics_descent(delta: float) -> void:
 	_horizontal_movement(delta)
 	move_and_slide()
 
-func _physics_ledge_descent(delta: float) -> void:
 	_check_grounded()
-	_horizontal_movement(delta)
-	move_and_slide()
