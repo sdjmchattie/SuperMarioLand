bcef5d4a27ad2230b625b43c9bd284b92deeaae8
Stuart McHattie <stuart.mchattie@gmail.com>
2026-02-16 21:11:31 +0000

    Add Points indicator scene

---
+++ scripts/points.gd
@@ -0,0 +1,26 @@
+extends Sprite2D
+
+@export var rise_velocity := 32.0
+@export var rise_seconds := 0.9
+
+@onready var free_timer: Timer = $FreeTimer
+
+func _ready() -> void:
+	free_timer.start(rise_seconds)
+
+func _process(delta: float) -> void:
+	position.y -= rise_velocity * delta
+
+func set_value(points: int) -> void:
+	# Map point value to sprite frame
+	match points:
+		100: frame = 0
+		200: frame = 1
+		400: frame = 2
+		500: frame = 3
+		800: frame = 4
+		1000: frame = 5
+		_: frame = 0  # Default to 100
+
+func _on_free_timer_timeout() -> void:
+	queue_free()
